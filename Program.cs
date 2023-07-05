b090e46f38b019e28fef120d1e748090f4605c3e
Sergey <samtriezo@gmail.com>
2023-07-05 22:08:20 +0700

    написал программу фильтрующую массив строк по условию длина <= 3

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+string[] FilterArray(string[] array)
+{
+    int sizeFilteredArray = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3) sizeFilteredArray++;
+    }
+
+    string[] filteredArray = new string[sizeFilteredArray];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            filteredArray[count] = array[i];
+            count++;
+        }
+    }
+    return filteredArray;
+}
+
+void ReadArray(string[] array)
+{
+    System.Console.Write("[");
+   for (int i = 0; i < array.Length; i++)
+   {
+        System.Console.Write(array[i]);
+        if(i < array.Length-1) System.Console.Write(", ");
+   }
+    System.Console.Write("]");
+}
+
+string[] arrayStr = new string[5];
+for (int i = 0; i < arrayStr.Length; i++)
+{
+    System.Console.Write($"Введите {i+1} строку: ");
+    arrayStr[i] = System.Console.ReadLine()!;
+}
+
+System.Console.Write("Текущий массив: ");
+ReadArray(arrayStr);
+
+System.Console.WriteLine();
+string[] filteredArray =  FilterArray(arrayStr);
+System.Console.Write("Отфильтрованный массив: ");
+ReadArray(filteredArray);
